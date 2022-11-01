2c1990d42b5f9d97db6d3c8fb2b6e9e2ffecc406
Alfredo <avnssena@gmail.com>
2022-11-01 03:45:35 -0300

    Melhorando a font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Poppins, sans-serif'
+        sans: 'Nunito, sans-serif'
       },
       fontSize: {
         xs: 13,
