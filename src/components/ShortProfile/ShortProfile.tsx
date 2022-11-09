dcba6fd8f7504c7fecf5f45ee7fcd1df9a727503
Alfredo <avnssena@gmail.com>
2022-11-09 02:58:12 -0300

    Alterando o nome do component shortProfile

--- src/components/ShortProfile/ShortProfile.tsx
+++ src/components/ShortProfile/ShortProfile.tsx
@@ -14,8 +14,8 @@ export function ShortProfile({ profilePic }: ShortProfileProps){
     <img src={profilePic} alt="Davide Biscuso" className="shortProfile__image rounded-full" />
 
     <div className="shortProfile__text">
-      <Heading color="black" size="sm" className="leading-4">Davide Biscuso</Heading>
-      <Text color="gray" size="lg" className="leading-4">@biscuttu</Text>
+      <Heading color="black" size="sm" className="leading-4">Bobur</Heading>
+      <Text color="gray" size="lg" className="leading-4">@bobur_mavlonov</Text>
     </div>
 
     <Icon icon="ellipses" className="justify-self-end hover:bg-dark-7 dark:hover:bg-dark-4 rounded-full p-2 box-content cursor-pointer" />
