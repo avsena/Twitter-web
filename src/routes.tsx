d3c6824068364244ea8607fd3a0d957b4378a9f2
Alfredo <avnssena@gmail.com>
2022-11-10 04:09:01 -0300

    Criando as rotas de páginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { DarkModeContextProvider } from './context/DarkModeContext/DarkModeContext'
 import { FeedContextProvider } from './context/FeedContext/FeedContext'
 import { Home } from './pages/Home'
+import Login from './pages/Login/Login'
 import { Profile } from './pages/Profile'
+import Signup from './pages/SignUp/Signup'
 //import { PageNotFound } from './pages/PageNotFound'
 
 import './styles/global.css'
@@ -14,9 +16,12 @@ export default function AppRouter() {
             <DarkModeContextProvider>
                 <FeedContextProvider>
                  <Routes>
-                    <Route path='/' element={<Home/>} />
+                    <Route path='/login' element={<Login/>} />
                     <Route path='/home' element={<Home/>} />
                     <Route path='/profile' element={<Profile/>} />
+                    <Route path='/signup' element={<Signup/>} />
+                    <Route path='/' element={<Profile/>} />
+
                  </Routes>
                 </FeedContextProvider>
             </DarkModeContextProvider>
