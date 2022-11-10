4e8cd51808d24a3f58143ee016f8595389eaecf0
Alfredo <avnssena@gmail.com>
2022-11-10 06:38:01 -0300

    Adicionando novas rotas de navegação

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { DarkModeContextProvider } from './context/DarkModeContext/DarkModeContext'
 import { FeedContextProvider } from './context/FeedContext/FeedContext'
-import { Home } from './pages/Home'
+import { Home } from './pages/Home/index'
 import Login from './pages/Login/Login'
-import { Profile } from './pages/Profile'
+import { Profile } from './pages/Profile/index'
 import Signup from './pages/SignUp/Signup'
+import { PageNotFound } from './pages/PageNotFound'
+import { Post } from './components/Post/Post'
 //import { PageNotFound } from './pages/PageNotFound'
 
 import './styles/global.css'
@@ -20,8 +22,7 @@ export default function AppRouter() {
                     <Route path='/home' element={<Home/>} />
                     <Route path='/profile' element={<Profile/>} />
                     <Route path='/signup' element={<Signup/>} />
-                    <Route path='/' element={<Profile/>} />
-
+                    <Route path='/' element={<PageNotFound/>} />
                  </Routes>
                 </FeedContextProvider>
             </DarkModeContextProvider>
