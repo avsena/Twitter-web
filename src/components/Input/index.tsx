c8560b6665d7d63d2a1f4b3e9eb3c627c6f91a39
Alfredo <avnssena@gmail.com>
2022-11-10 04:06:01 -0300

    Criando input de login

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,18 @@
+import {InputHTMLAttributes} from "react"
+
+interface inputPropos extends InputHTMLAttributes<HTMLInputElement> {
+    maxlength?: string,
+    placeholder?: string,
+    className: string,
+    rows? : string,
+    type: string
+  
+}
+
+function Input(props: inputPropos){
+    return(
+        <input {...props} />
+    )
+}
+
+export default Input
